d65a86b73e15d76f3d386c955d08b34fd731e609
Ubuntu <ubuntu@ip-172-31-19-109.ec2.internal>
2026-04-16 08:13:24 +0000

    Day 6: Added Nginx reverse proxy and UI update

--- app/app.py
+++ app/app.py
@@ -2,17 +2,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "CloudPulse CI/CD is working 🚀"
+    return """
+    <html>
+    <head>
+        <title>CloudPulse</title>
+        <style>
+            body {
+                background-color: #0f172a;
+                color: white;
+                text-align: center;
+                font-family: Arial;
+                padding-top: 100px;
+            }
+            h1 {
+                font-size: 50px;
+            }
+            p {
+                font-size: 20px;
+                color: #38bdf8;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>🚀 CloudPulse</h1>
+        <p>CI/CD Pipeline is Working Successfully</p>
+    </body>
+    </html>
+    """
 
-@app.route('/health')
-def health():
-    return "OK - CloudPulse is running"
-
-@app.route('/about')
-def about():
-    return "CloudPulse DevOps Project by Muhammed"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
