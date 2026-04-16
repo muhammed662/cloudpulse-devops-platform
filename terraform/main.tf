192830da5b5bb0c6800e2b2daf1f9c8af188a43e
Muhammed <muhammedlatheef662@gmail.com>
2026-04-16 19:12:05 +0000

    changed terraform user data making it reproducable

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,26 +56,46 @@ resource "aws_instance" "cloudpulse" {
   vpc_security_group_ids = [aws_security_group.cloudpulse_sg.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              set -eux
+#!/bin/bash
+set -eux
 
-              apt-get update -y
-              apt-get install -y docker.io git
+apt-get update -y
+apt-get install -y docker.io git nginx
 
-              systemctl enable docker
-              systemctl start docker
+systemctl enable docker
+systemctl start docker
 
-              usermod -aG docker ubuntu
+systemctl enable nginx
 
-              cd /home/ubuntu
-              rm -rf cloudpulse-devops-platform
-              git clone https://github.com/muhammed662/cloudpulse-devops-platform.git
+usermod -aG docker ubuntu
 
-              cd /home/ubuntu/cloudpulse-devops-platform
-              docker rm -f cloudpulse-container || true
-              docker build -t cloudpulse-app .
-              docker run -d --name cloudpulse-container -p 80:5000 cloudpulse-app
-              EOF
+cd /home/ubuntu
+rm -rf cloudpulse-devops-platform
+git clone https://github.com/muhammed662/cloudpulse-devops-platform.git
+
+cd /home/ubuntu/cloudpulse-devops-platform
+docker rm -f cloudpulse-container || true
+docker build -t cloudpulse-app .
+docker run -d --name cloudpulse-container -p 5000:5000 cloudpulse-app
+
+cat > /etc/nginx/sites-available/default <<'NGINXCONF'
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+NGINXCONF
+
+nginx -t
+systemctl restart nginx
+EOF
 
   tags = {
     Name = "CloudPulse-Terraform"
