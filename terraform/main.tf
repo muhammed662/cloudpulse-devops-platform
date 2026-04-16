9e6a93f40811506050448c89e1c61f8e62db8768
Muhammed <muhammedlatheef662@gmail.com>
2026-04-16 13:04:44 +0000

    Automated app setup with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "cloudpulse_sg" {
   name        = "cloudpulse-terraform-sg"
   description = "Allow SSH and HTTP"
@@ -35,9 +50,32 @@ resource "aws_security_group" "cloudpulse_sg" {
 }
 
 resource "aws_instance" "cloudpulse" {
-  ami                    = "ami-0c02fb55956c7d316"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
+  key_name               = "cloudpulse-key"
   vpc_security_group_ids = [aws_security_group.cloudpulse_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -eux
+
+              apt-get update -y
+              apt-get install -y docker.io git
+
+              systemctl enable docker
+              systemctl start docker
+
+              usermod -aG docker ubuntu
+
+              cd /home/ubuntu
+              rm -rf cloudpulse-devops-platform
+              git clone https://github.com/muhammed662/cloudpulse-devops-platform.git
+
+              cd /home/ubuntu/cloudpulse-devops-platform
+              docker rm -f cloudpulse-container || true
+              docker build -t cloudpulse-app .
+              docker run -d --name cloudpulse-container -p 80:5000 cloudpulse-app
+              EOF
 
   tags = {
     Name = "CloudPulse-Terraform"
