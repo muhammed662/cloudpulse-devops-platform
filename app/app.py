ca82035242cd0cd7651e9c31ff22cb866626a1f0
Muhammed <muhammedlatheef662@gmail.com>
2026-04-16 18:55:02 +0000

    Testing nginx reverse proxy setup

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ def home():
     </head>
     <body>
         <h1>🚀 CloudPulse</h1>
-        <p>CloudPulse is the Future 🚀</p>
+        <p>CloudPulse is the Future of Devops 🚀</p>
     </body>
     </html>
     """
